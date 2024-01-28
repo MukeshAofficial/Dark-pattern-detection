e33aa47967e4c8911b253bd92b49fcbaebd88808
Mukesh A <132742860+MukeshAofficial@users.noreply.github.com>
2024-01-28 20:56:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    const resultDiv = document.getElementById('result');
-
-    form.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const url = document.getElementById('url').value;
-        const response = await fetch('/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: `url=${encodeURIComponent(url)}`
-        });
-
-        const result = await response.json();
-        resultDiv.innerHTML = result.result;
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    var analyzeButton = document.getElementById('analyzeButton');
+    analyzeButton.addEventListener('click', showResults);
 });
+
+function showResults() {
+    document.getElementById("analysis-section").style.display = "none";
+    document.getElementById("result-section").style.display = "block";
+}
